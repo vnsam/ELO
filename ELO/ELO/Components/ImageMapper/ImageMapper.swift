b98dcba4e2cd1dc5db0010557f5575d0c9fff277
Vignesh Narayanasamy <Vignesh.Narayanasamy@emc.com>
2018-05-06 09:41:17 +0530

    Added ImageMapper Object

---
+++ ELO/ELO/Components/ImageMapper/ImageMapper.swift
@@ -0,0 +1,18 @@
+//
+//  ImageMapper.swift
+//  ELO
+//
+//  Created by Narayanasamy, Vignesh on 5/6/18.
+//  Copyright © 2018 Infinitum Inc. All rights reserved.
+//
+
+import Foundation
+
+class ImageMapper {
+    let imageDicationary: [String: String] = ["Renly Baratheon": "Renly",
+                                              "Mance Rayder": "Mance",
+                                              "Stannis Baratheon": "Stannis",
+                                              "Balon/Euron Greyjoy": "Greyjoy",
+                                              "Robb Stark": "Stark",
+                                              "Joffrey/Tommen Baratheon": "Lannister"]
+}
